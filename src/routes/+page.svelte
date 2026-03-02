766757b8d2b8e7f4903e41904ba4028fa1641261
Revilo0509 <olivers_gamecenter@outlook.com>
2026-03-02 22:50:07 +0100

    Configured

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import Button from "$lib/components/ui/button/button.svelte";
+
+    function sayHello() {
+        console.log("Hello")
+    }
+</script>
+
+<Button onclick={sayHello}>Hello</Button>
