397785f3ce01d5d9d9674a5046b0fed32f4b1562
Revilo0509 <olivers_gamecenter@outlook.com>
2026-03-02 23:31:25 +0100

    Created Navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,29 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import SGClogo from '$lib/assets/SGC-logo.webp';
+
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<div class="shadow-xl">
+	<Navbar>
+		<NavBrand href="/">
+			<img src={SGClogo} alt="logo" class="h-12" />
+		</NavBrand>
+		<NavHamburger />
+		<NavUl>
+			<NavLi class="text-xl" href="/kalender">Kalendern</NavLi>
+			<NavLi class="text-xl" href="/servrar">Servrar</NavLi>
+			<NavLi class="text-xl" href="/om-oss">Om oss</NavLi>
+			<NavLi class="text-xl" href="/sociala-medier">Sociala Medier</NavLi>
+		</NavUl>
+	</Navbar>
+</div>
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import Button from "$lib/components/ui/button/button.svelte";
+    import { Button } from "flowbite-svelte";
 
     function sayHello() {
         console.log("Hello")
