19d1648e7edc7dc3c35c125fa8cd7909e4e5e08d
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2026-03-03 13:11:37 +0100

    Add pocketbase example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
     import { Button } from "flowbite-svelte";
+    let { data } = $props();
 
     function sayHello() {
         console.log("Hello")
     }
 </script>
 
-<Button onclick={sayHello}>Hello</Button>
+<div class="p-4 flex flex-col gap-4">
+    {#each data.homePagePosts as post}
+        <h2>{post.text}</h2>
+    {/each}
+</div>
