48f4b67be246a010c3a880348945037ffd8da95b
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2026-03-03 13:33:17 +0100

    Dark and Light mode button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,14 +3,24 @@
 	import favicon from '$lib/assets/favicon.svg';
 	import SGClogo from '$lib/assets/SGC-logo.webp';
 
-	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button } from 'flowbite-svelte';
+	import { ModeWatcher } from 'mode-watcher';
+	import { toggleMode } from 'mode-watcher';
 
 	let { children } = $props();
+	let isDark = $state(false);
+
+	function toggleTheme() {
+		toggleMode();
+		isDark = !isDark;
+	}
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+<ModeWatcher />
 
 <div class="shadow-xl">
 	<Navbar>
@@ -24,6 +34,8 @@
 			<NavLi class="text-xl" href="/om-oss">Om oss</NavLi>
 			<NavLi class="text-xl" href="/sociala-medier">Sociala Medier</NavLi>
 		</NavUl>
+		<Button onclick={toggleTheme} class="ml-4 dark:bg-secondary-50 dark:hover:bg-secondary-50">{isDark ? '☀️' : '🌙'}</Button>
 	</Navbar>
 </div>
+
 {@render children()}
