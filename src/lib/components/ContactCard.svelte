92e55c2618de5b4f6a75658a59d30cfdfa8aa922
Revilo0509 <olivers_gamecenter@outlook.com>
2026-03-05 21:52:52 +0100

    Små fix igen

--- src/lib/components/ContactCard.svelte
+++ src/lib/components/ContactCard.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { ComponentType, SvelteComponent } from 'svelte';
-	type IconProp = ComponentType<SvelteComponent> | string;
+	import type { SimpleIcon } from 'simple-icons';
+	type IconProp = ComponentType<SvelteComponent> | string | SimpleIcon;
 
 	let { name, Icon, link } = $props<{ name: string; Icon: IconProp; link: string }>();
 </script>
@@ -9,6 +10,8 @@
 	<a class="flex flex-col items-center p-4 duration-300 hover:scale-110" href={link}>
 		{#if typeof Icon === 'string'}
 			<img src={Icon} alt={name} class="h-32 w-32" />
+		{:else if 'svg' in Icon}
+			<div class="h-32 w-32">{@html Icon.svg.replace('<svg', '<svg fill="currentColor"')}</div>
 		{:else}
 			<Icon class="h-32 w-32" />
 		{/if}
--- src/routes/kontakt/+page.svelte
+++ src/routes/kontakt/+page.svelte
@@ -1,20 +1,21 @@
 <script>
 	import ContactCard from '$lib/components/ContactCard.svelte';
-	import { MessageCircle, Mail, Facebook, Github, Instagram, Youtube } from 'lucide-svelte';
-	import SverokIconBlack from '$lib/assets/sverok_black.svg';
-	import SverokIconWhite from '$lib/assets/sverok_white.svg';
+	import { Mail } from 'lucide-svelte';
+	import { siDiscord, siX, siGithub, siInstagram, siYoutube, siFacebook } from 'simple-icons';
+	import SverokIconBlack from '$lib/assets/icons/sverok_black.svg';
+	import SverokIconWhite from '$lib/assets/icons/sverok_white.svg';
 	import { mode } from 'mode-watcher';
 </script>
 
 <div class="flex items-center justify-center">
 	<div class="grid grid-cols-1 gap-6 p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-		<ContactCard name="Discord" Icon={MessageCircle} link="https://discord.sgc.se" />
-		<ContactCard name="X" Icon={MessageCircle} link="https://x.com/sgckontot/" />
-		<ContactCard name="Github" Icon={Github} link="https://github.com/onlinesgc" />
-		<ContactCard name="Instagram" Icon={Instagram} link="https://www.instagram.com/sgckontot/" />
-		<ContactCard name="Youtube" Icon={Youtube} link="https://www.youtube.com/@onlineforeningensgc" />
+		<ContactCard name="Discord" Icon={siDiscord} link="https://discord.sgc.se" />
+		<ContactCard name="X" Icon={siX} link="https://x.com/sgckontot/" />
+		<ContactCard name="Github" Icon={siGithub} link="https://github.com/onlinesgc" />
+		<ContactCard name="Instagram" Icon={siInstagram} link="https://www.instagram.com/sgckontot/" />
+		<ContactCard name="Youtube" Icon={siYoutube} link="https://www.youtube.com/@onlineforeningensgc" />
 		<ContactCard name="Bli medlem" Icon={mode.current === 'dark' ? SverokIconWhite : SverokIconBlack} link="https://sverok.se" />
-		<ContactCard name="Facebook" Icon={Facebook} link="https://www.facebook.com/people/Onlinef%C3%B6reningen-SGC/61554997371254/" />
+		<ContactCard name="Facebook" Icon={siFacebook} link="https://www.facebook.com/people/Onlinef%C3%B6reningen-SGC/61554997371254/" />
 		<ContactCard name="Email" Icon={Mail} link="mailto:kontakt@sgc.se" />
 	</div>
 </div>
