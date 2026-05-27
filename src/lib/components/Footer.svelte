120db2cc2efc0ecbf872e3996911b183a772362b
lukas <67807954+lukasabbe@users.noreply.github.com>
2026-05-27 07:39:07 +0200

    Added in lysator logo to website

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import LysatorLogo from '$lib/assets/logo-no-bg.svg';
 	let date = new Date();
 </script>
 
@@ -7,6 +8,10 @@
 		<p class="text-sm text-gray-500 dark:text-gray-400">
 			&copy; {date.getFullYear()} Onlineföreningen SGC. All rights reserved.
 		</p>
+		<a href="https://www.lysator.liu.se" class="flex items-center gap-2 text-gray-500 hover:text-primary-500 dark:text-gray-400">
+			<span class="text-sm sm:text-base">Våra Minecraft servrar är inhysta hos</span>
+			<img src={LysatorLogo} alt="Lysator" class="h-7 sm:h-9" />
+		</a>
 		<div class="flex gap-4">
 			<a href="/information/om-oss" class="text-sm text-gray-500 hover:text-primary-500 dark:text-gray-400">Om Oss</a>
 			<a href="/kontakt" class="text-sm text-gray-500 hover:text-primary-500 dark:text-gray-400">Kontakt</a>
